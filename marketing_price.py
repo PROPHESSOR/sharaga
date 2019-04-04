4680580762f299f65f2540c5cccedfa3345ee7b5
PROPHESSOR <xenteze@gmail.com>
2019-04-04 10:13:31 +0300

    04.04.2019 - Пара Маркетинга (лекция + прога)

---
+++ marketing_price.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# coding: utf-8
+
+def percent(x):
+    return float(x) / 100.0
+
+def calculate(KB, TBp, BPp, CMp, duty, OMPp, AZp, overprice, USD=27.06, PDVp=20):
+    TB = KB * percent(TBp)
+    print 'TB =', TB
+    BP = (KB + TB) * percent(BPp) 
+    print 'ВП =', BP
+    CM = (KB + TB + BP) * percent(CMp)
+    print 'СМ =', CM
+    MB = KB + TB + BP + CM
+    print 'МВ =', MB
+    MB *= USD
+    print 'МВ (грн) =', MB
+    OMP = MB * percent(duty)
+    print 'ОМП =', OMP
+    BM = MB * percent(OMPp)
+    print 'ВМ =', BM
+    AZ = (MB + OMP + BM) * percent(AZp)
+    print 'АЗ =', AZ
+    PDV = (MB + OMP + BM + AZ) * percent(PDVp)
+    print 'ПДВ =', PDV
+    CP = MB + OMP + BM + AZ + PDV
+    print 'ЦП =', CP
+    RC = CP + (CP * percent(overprice))
+    print 'РЦ =', RC
+
+if __name__ == "__main__":
+    calculate(912, 10, 5, 14, 0.2, 10, 15, 25)
