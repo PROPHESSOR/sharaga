81559bd6daa33ee9b6e6bd1d23672369bb673309
PROPHESSOR <xenteze@gmail.com>
2019-04-25 10:18:11 +0300

    Пара Маркетинга 25.04.2019
    Нихера не понятно, но я как-то умудрился сделать совпадение первых двух
    колонок таблицы

---
+++ marketing_margprofit.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+
+PRICE_BUY = 3000
+PRICE_SELL = 10000
+CONST = 21000
+PROFIT = 25000 # Желаемый доход
+
+def marginalProfit(price, var, const):
+    ''' price - выручка (цена); var - переменные траты; const - постоянные траты '''
+    print 'Рассчёт маржинального дохода при цене', price, 'пер. тратам', var, 'пост. тратам', const
+    md = price - var
+    print 'МД =', md
+    x = int(round(float(const) / float(md)))
+    print 'x =', x, 'шт.'
+    return (x, md)
+
+def getPoint(price, var, const):
+    ''' price - цена продажи; var - переменные траты; const - постоянные траты '''
+    x = marginalProfit(price, var, const)[0]
+    out = price * x
+    print 'Точка безубыточности =', out
+    return out
+
+def zapasBezopasnosti(price, var, const): # FIXME:
+    print '=== Рассчитываем запас безопасности ==='
+    point = getPoint(price, var, const)
+    zbez = price - point
+    print 'Запас безопасности =', zbez
+    k = float(zbez) / float(price)
+    print 'Коэф. з.б. =', k
+
+# zapasBezopasnosti(10000, 3000, 21000)
+
+
+def buildTable(price, var, const, Profit, count=None):
+    if count == None:
+        count = marginalProfit(price, var, const + Profit)[0] # Количество, чтобы выйти в 0
+    print 'Строим таблицу продажи', count, 'шт. по цене', price, 'потратив', const, 'в общем +', var, 'на штуту'
+    # x = marginalProfit(price, var, const + Profit)[0]
+    xprice = price * count
+    print 'Выручка', xprice
+    Var = count * var
+    print 'Пер. траты', Var
+    md = xprice - Var
+    print 'Марж. дох.', md
+    print 'Пост. траты', const
+    profit = md - const
+    print 'Прибыль', profit
+    duty = profit * 0.18
+    print 'Налог', duty
+    cp = profit - duty
+    print 'Чист. приб.', cp
+
+marginalProfit(PRICE_SELL, PRICE_BUY, CONST)
+marginalProfit(PRICE_SELL, PRICE_BUY, CONST + PROFIT)
+
+buildTable(PRICE_SELL, PRICE_BUY, CONST, PROFIT)
+buildTable(PRICE_SELL, PRICE_BUY, CONST, PROFIT, 8) # 8 x 3000
+# buildTable(PRICE_SELL - PRICE_SELL * 0.12, PRICE_BUY, CONST, PROFIT, 8 + 8 * 0.16) # 8 x 3000
